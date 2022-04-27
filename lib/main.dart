e878eb0052924dbfbdf49c24e7b219ed4a33c720
lokeshbs1994 <lokeshbs619@gmail.com>
2022-04-27 09:32:24 +0530

    login page , connection to firebase

---
+++ lib/main.dart
@@ -0,0 +1,181 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'profile_dart.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: const HomePage(),
+    );
+  }
+}
+
+/*
+  TODO list:
+  Step 1 : create the main layout of the app (only the UI)
+  Step 2 : Login to your firebase console (if you dont have an account you can create one, It is easy and free)
+  Step 3 : Create a new firebaseproject
+  Step 4 : Lets Add firebase dependencies for flutter
+  step 5 : Let's init the Firebase app
+  step 6 : Create the Login function 
+  Step 7 : now let's create a new user inside our base and test the app
+  Step 8 : let's input our user data and test the app
+ */
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+//Initialize Firebase App
+  Future<FirebaseApp> _initializeFirebase() async {
+    FirebaseApp firebaseApp = await Firebase.initializeApp();
+    return firebaseApp;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder(
+        future: _initializeFirebase(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return LoginScreen();
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  //Login function
+  static Future<User?> loginUsingEmailPassword(
+      {required String email,
+      required String password,
+      required BuildContext context}) async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user;
+    try {
+      UserCredential userCredential = await auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      user = userCredential.user;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == "user-not-found") {
+        print("No User found for that email");
+      }
+    }
+    return user;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // create a textfield controller
+    TextEditingController _emailController = TextEditingController();
+    TextEditingController _passwordController = TextEditingController();
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            "MyApp Title",
+            style: TextStyle(
+                color: Colors.black, fontSize: 28, fontWeight: FontWeight.bold),
+          ),
+          const Text(
+            "Login to Your App",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 42,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(
+            height: 44.0,
+          ),
+          TextField(
+            controller: _emailController,
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(
+              hintText: "User Email",
+              prefixIcon: Icon(Icons.mail, color: Colors.black),
+            ),
+          ),
+          const SizedBox(
+            height: 26.0,
+          ),
+          TextField(
+            controller: _passwordController,
+            obscureText: true,
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(
+              hintText: "User Password",
+              prefixIcon: Icon(Icons.lock, color: Colors.black),
+            ),
+          ),
+          const SizedBox(
+            height: 12.0,
+          ),
+          const Text(
+            "Don't Remember your Password?",
+            style: TextStyle(color: Colors.blue),
+          ),
+          const SizedBox(height: 88.0),
+          Container(
+            width: double.infinity,
+            child: RawMaterialButton(
+              fillColor: const Color(0xFF0069FE),
+              elevation: 0.0,
+              padding: const EdgeInsets.symmetric(vertical: 20.0),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12.0)),
+              onPressed: () async {
+                // lets test the app
+                User? user = await loginUsingEmailPassword(
+                    email: _emailController.text,
+                    password: _passwordController.text,
+                    context: context);
+                print(user);
+                if (user != null) {
+                  Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (context) => ProfileScreen()));
+                  // lets make a new screen
+                }
+              },
+              child: const Text(
+                "Login",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18.0,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
